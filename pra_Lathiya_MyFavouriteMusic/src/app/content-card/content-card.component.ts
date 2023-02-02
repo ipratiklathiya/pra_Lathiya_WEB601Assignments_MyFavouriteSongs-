09da1badd172206cac4bcb0cd2ccb901e96a360e
ipratiklathiya <w0791684@myscc.ca>
2023-02-02 00:07:01 -0500

    delete the contentlist class implemantation and delete the content-list.ts file
    delete the contentlist class implemantation and delete the content-list.ts file

--- pra_Lathiya_MyFavouriteMusic/src/app/content-card/content-card.component.ts
+++ pra_Lathiya_MyFavouriteMusic/src/app/content-card/content-card.component.ts
@@ -1,35 +1,10 @@
 import { Component } from '@angular/core';
-import { ContentList } from './helper-files/content-list';
 
 @Component({
-  selector: 'app-content-card',
+  selector: 'app-root',
   template: `
-    <div *ngFor="let content of contentList.List; let i = index">
-      <div [innerHTML]="contentList.display(i)"></div>
-    </div>
+    <app-content-card></app-content-card>
   `,
 })
-export class ContentCardComponent {
-  contentList = new ContentList();
-
-  constructor() {
-    this.contentList.add({
-      id: 1,
-      title: 'First Content',
-      description: 'This is the first content',
-      creator: 'John Doe',
-    });
-    this.contentList.add({
-      id: 2,
-      title: 'Second Content',
-      description: 'This is the second content',
-      creator: 'Jane Doe',
-    });
-    this.contentList.add({
-      id: 3,
-      title: 'Third Content',
-      description: 'This is the third content',
-      creator: 'John Smith',
-    });
-  }
+export class AppComponent {
 }
