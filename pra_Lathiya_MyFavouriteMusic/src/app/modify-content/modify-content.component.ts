c0b93e0d6693369610bf45e4bc72e22fe282be45
ipratiklathiya <w0791684@myscc.ca>
2023-03-29 18:51:23 -0400

    added the bonus part

--- pra_Lathiya_MyFavouriteMusic/src/app/modify-content/modify-content.component.ts
+++ pra_Lathiya_MyFavouriteMusic/src/app/modify-content/modify-content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Content } from '../content';
-import { ContentService } from '../content.service';
+import { ContentService } from '../content-service.service';
 import { MessageService } from '../message.service';
 
 @Component({
@@ -9,34 +9,49 @@ import { MessageService } from '../message.service';
   styleUrls: ['./modify-content.component.css']
 })
 export class ModifyContentComponent implements OnInit {
-  newContent: Content = {
-    id: null,
-    title: '',
-    description: '',
-    creator:'',
-    imgUrl: '',
-    type:'',
-    tags:''
-  };
 
-  constructor(private contentService: ContentService, private messageService: MessageService) { }
+  content: Content = { id: null, title: '', description: '', imgUrl: '', creator: '', type:'', tags:''  };
+  updateMode: boolean = false;
 
-  ngOnInit() {
+  constructor(
+    private contentService: ContentService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
   }
 
   addContent(): void {
-    this.contentService.addContent(this.newContent)
-      .subscribe(content => {
-        this.messageService.add(`Content added with id=${content.id}`);
-        this.newContent = {
-          id: null,
-          title: '',
-          description: '',
-          creator:'',
-          imgUrl: '',
-          type:'',
-          tags:''
-        };
+    if (!this.content.id) {
+      this.contentService.addContent(this.content).subscribe(() => {
+        this.messageService.add('Content added successfully');
+        this.clearFields();
       });
+    } else {
+      this.updateMode = true;
+      const contentId = this.content.id;
+      this.contentService.getContent(contentId).subscribe(content => {
+        if (content) {
+          this.content = content;
+          this.messageService.add(`Updating content with id ${contentId}`);
+        } else {
+          this.updateMode = false;
+          this.messageService.add(`No content found with id ${contentId}`);
+        }
+      });
+    }
+  }
+
+  updateContent(): void {
+    const contentId = this.content.id;
+    this.contentService.updateContent(contentId, this.content).subscribe(() => {
+      this.messageService.add(`Content with id ${contentId} updated successfully`);
+      this.clearFields();
+      this.updateMode = false;
+    });
+  }
+
+  clearFields(): void {
+    this.content = { id: null, title: '', description: '', imgUrl: '', creator: '', type:'', tags:'' };
   }
 }
